05c9aee695b62b1b7360c86737e91b9f28fab29e
Maxim345525 <maximmaksimchyk@gmail.com>
2023-02-12 19:17:30 +0200

    add code

--- main.cpp
+++ main.cpp
@@ -1,1 +1,23 @@
-u
+#include <iostream> 
+using namespace std;
+int main()
+{
+	int a, n;
+	cout << "Input number a:";
+	cin >> a;
+	cout << "Input stepin n:";
+	cin >> n;
+	int result = 1;
+	if (n == 0)
+	{
+		cout << result;
+	}
+	else
+	{
+		for (int i = 1; i <= n; i++)
+		{
+			result *= a;
+		}
+		cout << result;
+	}
+}
