34234ef638c3e4f3e7ef932ce5d4de5c92f25a1a
Maxim345525 <maximmaksimchyk@gmail.com>
2023-02-12 19:27:11 +0200

    add code

--- main.cpp
+++ main.cpp
@@ -1,1 +1,14 @@
-u
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cout << "Input Number-->";
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cout << " * ";
+	}
+	cout << endl;
+	return 0;
+}
