3e183d15beb4c40249f425d27b53859d3e1b0ecc
Maxim345525 <maximmaksimchyk@gmail.com>
2023-02-13 16:23:25 +0200

    add code

--- main.cpp
+++ main.cpp
@@ -1,1 +1,20 @@
-u
+#include <iostream>
+int sum(int a, int b)
+{
+    if (a - b == 1)
+      return 0;
+    return b + sum(a, b - 1);
+}
+int main()
+{
+    int a;
+    std::cout << "Enter start: ";
+    std::cin >> a;
+    int b;
+    std::cout << "Enter end: ";
+    std::cin >> b;
+    if (a == b || a > b)
+        return 0;
+    std::cout << "The sum is " << sum(a, b);
+    return 0;
+}
